9cbffb898a616793bd591fccefba2e0964aefd1b
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-08 10:07:42 +0800

    try edit index

--- index.php
+++ index.php
@@ -7,18 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KMS</title>
     <link rel="stylesheet" href="style.css">
-    <style>
-        table,
-        th,
-        td {
-            border: 1px solid black;
-        }
-
-        th,
-        td {
-            padding: 10px;
-        }
-    </style>
 </head>
 
 <body>
