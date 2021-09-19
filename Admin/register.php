b4035dcd87cc53a6db79c9b8d1d87f1f1a5ca27b
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-19 16:38:34 +0800

    try edit index

--- Admin/register.php
+++ Admin/register.php
@@ -10,42 +10,38 @@ require '../connection.php';
 </head>
 
 <body>
-    <div class="container">
-        <br><br><br>
-        <div class="center">
-            <h2>Kindergarten Management System</h2>
-        </div>
-        <center><br> Nama Programmer: Admin (Aliff)</a></center>
-    </div>
     <center>
+        <h2>System Name : Kindergarden Management System(KMS)</h2><br>
+        <br>Nama Programmer :<a href="admin/">Admin(Aliff)</a>
+
         <h2>Register Guru</h2>
+
+        <form action="index.php" method="post">
+
+            <table align="center">
+                <tr>
+                    <td>IdGuru :</td>
+                    <td> <input type="text" name="idguru"> </td>
+                </tr>
+                <tr>
+                    <td>Nama Penuh :</td>
+                    <td> <input type="text" name="fullname"> </td>
+                </tr>
+                <tr>
+                    <td>Password :</td>
+                    <td> <input type="text" name="password"> </td>
+                </tr>
+                <tr>
+                <tr>
+                    <td colspan="2">
+                        <center>
+                            <input type="submit" value="Add" />
+                        </center>
+                    </td>
+                </tr>
+            </table>
+        </form>
     </center>
-    <form action="index.php" method="post">
-
-        <table align="center">
-            <tr>
-                <td>IdGuru :</td>
-                <td> <input type="text" name="idguru"> </td>
-            </tr>
-            <tr>
-                <td>Nama Penuh :</td>
-                <td> <input type="text" name="fullname"> </td>
-            </tr>
-            <tr>
-                <td>Password :</td>
-                <td> <input type="text" name="password"> </td>
-            </tr>
-            <tr>
-            <tr>
-                <td colspan="2">
-                    <center>
-                        <input type="submit" value="Add"/>
-                    </center>
-                </td>
-            </tr>
-        </table>
-    </form>
-
 </body>
 
 </html>
