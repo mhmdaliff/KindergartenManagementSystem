1db7ea8f00655f8b55f77c1f5efbce9f9e0de4d7
mhmdaliff <aliffmuhammad640@gmail.com>
2021-09-19 17:33:24 +0800

    try edit index

--- Admin/save.php
+++ Admin/save.php
@@ -5,52 +5,24 @@ require '../connection.php';
 $sql = "SELECT * FROM guru";
 $result = mysqli_query($conn, $sql);
 header("location:index.php");
+
+if (mysqli_num_rows($result) > 0) {
+
+
+    while ($row = mysqli_fetch_assoc($result)) {
 ?>
 
-<html>
+        <tr>
+            <td><?php echo $row["idguru"]; ?></td>
+            <td><?php echo $row["idname"]; ?></td>
+            <td><?php echo $row["password"]; ?></td>
+        </tr>
 
-<head>
-    <title>KMS</title>
-    <link rel="stylesheet" href="../style.css">
-</head>
+<?php
 
-<body>
-    <center>
-        <h2>System Name : Kindergarden Management System(KMS)</h2><br>
-        <br>Nama Programmer :<a href="admin/">Admin(Aliff)</a>
-
-        <br>
-        <table border="1">
-            <tr>
-                <th>IdGuru</th>
-                <th>Nama Penuh</th>
-                <th>Password</th>
-            </tr>
-
-            <?php
-            if (mysqli_num_rows($result) > 0) {
-
-
-                while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-
-                    <tr>
-                        <td><?php echo $row["idguru"]; ?></td>
-                        <td><?php echo $row["idname"]; ?></td>
-                        <td><?php echo $row["password"]; ?></td>
-                    </tr>
-
-            <?php
-
-                }
-            } else {
-                echo "0 results";
-            }
-            mysqli_close($conn);
-            ?>
-
-        </table>
-    </center>
-</body>
-
-</html>
+    }
+} else {
+    echo "0 results";
+}
+mysqli_close($conn);
+?>
