187dbe39f766e8ed6b11b67a22bbdb9a2ed0b4ee
merisaran <merisanik00@gmail.com>
2021-09-21 15:21:20 +0800

    pertama kali push

--- kindergartenmanagementsystem.sql
+++ kindergartenmanagementsystem.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2021 at 02:27 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.30
+-- Generation Time: Sep 21, 2021 at 09:19 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,21 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `anak`
+--
+
+CREATE TABLE `anak` (
+  `idanak` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `noic` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `guru`
 --
 
 CREATE TABLE `guru` (
-  `idguru` varchar(40) NOT NULL,
-  `idname` varchar(40) NOT NULL,
-  `password` varchar(11) NOT NULL
+  `idguru` int(11) NOT NULL,
+  `idname` int(11) NOT NULL,
+  `password` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `guru`
---
-
-INSERT INTO `guru` (`idguru`, `idname`, `password`) VALUES
-('A001', 'Muhammad Aliff Bin Abdul Haris', '1234567');
 
 -- --------------------------------------------------------
 
@@ -51,12 +57,18 @@ CREATE TABLE `ibubapa` (
   `id_name` int(30) NOT NULL,
   `password` int(11) NOT NULL,
   `alamat` varchar(30) NOT NULL,
-  `no_telefon` varchar(11) NOT NULL
+  `no_telefon` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `anak`
+--
+ALTER TABLE `anak`
+  ADD PRIMARY KEY (`idanak`);
 
 --
 -- Indexes for table `guru`
@@ -69,6 +81,22 @@ ALTER TABLE `guru`
 --
 ALTER TABLE `ibubapa`
   ADD PRIMARY KEY (`id_ibubapa`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anak`
+--
+ALTER TABLE `anak`
+  MODIFY `idanak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `ibubapa`
+--
+ALTER TABLE `ibubapa`
+  MODIFY `id_ibubapa` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
